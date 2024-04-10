e65eeae3a26bc68c2588126de3b4613e427d2f6b
randall505 <157840223+randall505@users.noreply.github.com>
2024-04-10 15:21:46 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+// smooth scrolling
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+
+//copy right live year
+let yearEl = new Date().getUTCFullYear()
+console.log(yearEl)
+document.getElementById('year').innerHTML = yearEl;
